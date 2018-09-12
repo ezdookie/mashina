8d89b4a14c9a5e1a9cc3e7d3c63259356526702d
Brian <brian@bleax.com>
2018-09-12 11:59:02 -0500

    add silent parameter to import_string

--- mashina/routing/base.py
+++ mashina/routing/base.py
@@ -1,8 +1,9 @@
-from importlib import import_module
+from mashina.utils.misc import import_string
 from config import settings
 
 root_routes = []
 
 for app in settings.APPS:
-    app_routes_module = import_module('%s.routes' % app)
-    root_routes.extend(getattr(app_routes_module, 'routes'))
+    routes = import_string('%s.routes.routes' % app, silent=True)
+    if routes is not None:
+        root_routes.extend(routes)
--- mashina/utils/misc.py
+++ mashina/utils/misc.py
@@ -1,7 +1,17 @@
 from importlib import import_module
 
 
-def import_string(dotted_path):
-    module_path, class_name = dotted_path.rsplit('.', 1)
-    module = import_module(module_path)
-    return getattr(module, class_name)
+def import_string(dotted_path, silent=False):
+    try:
+        module_path, class_name = dotted_path.rsplit('.', 1)
+        module = import_module(module_path)
+        attr = getattr(module, class_name)
+    except ModuleNotFoundError:
+        if not silent:
+            raise ModuleNotFoundError
+        attr = None
+    except AttributeError:
+        if not silent:
+            raise AttributeError
+        attr = None
+    return attr
