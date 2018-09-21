aa9f088b140d6a4af84966f8f11e1fd95c46577f
Brian <brian@bleax.com>
2018-09-21 16:21:58 -0500

    add uuid field support

--- mashina/models/mixins.py
+++ mashina/models/mixins.py
@@ -1,6 +1,12 @@
+import uuid
 from sqlalchemy import Column, Integer
+from sqlalchemy.dialects.postgresql import UUID
+from mashina.models.custom import GUID
 
 
 class ModelIntegerIDMixin(object):
-
     id = Column(Integer, primary_key=True)
+
+
+class ModelGUIDMixin(object):
+    id = Column(GUID, primary_key=True, default=uuid.uuid4)
--- mashina/utils/serialize.py
+++ mashina/utils/serialize.py
@@ -1,11 +1,15 @@
+import uuid
 import datetime
 import decimal
+import uuid
 
 
 def json_serializer(obj):
     if isinstance(obj, datetime.datetime):
         return str(obj)
     elif isinstance(obj, decimal.Decimal):
+        return str(obj)
+    elif isinstance(obj, uuid.UUID):
         return str(obj)
 
     raise TypeError('Cannot serialize {!r} (type {})'.format(obj, type(obj)))
