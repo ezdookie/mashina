56b551006cd8d73fef3c0adf2b56d9df608d0bcb
ezdookie <brian@bleax.com>
2018-10-18 08:34:14 -0500

    remove test code

--- mashina/middlewares/jwt_auth.py
+++ mashina/middlewares/jwt_auth.py
@@ -5,12 +5,12 @@ from config import settings
 
 class JWTAuthMiddleware(object):
     def process_resource(self, req, resp, resource, params):
-        try:
-            r = redis.StrictRedis(host='redis', db=0)
-            token = req.auth.split(' ')
-            token = r.get(token[1])
-        except:
-            token = None
-        # token = req.auth
+        # try:
+        #     r = redis.StrictRedis(host='redis', db=0)
+        #     token = req.auth.split(' ')
+        #     token = r.get(token[1])
+        # except:
+        #     token = None
+        token = req.auth
         if token is not None:
             req.context['auth'] = jwt.decode(token, settings.JWT_SECRET_KEY, algorithm='HS256')
