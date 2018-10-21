19e5605db35f2185706ed0bb51769ff880d000af
ezdookie <brian@bleax.com>
2018-10-21 00:31:44 -0500

    remove redis import

--- mashina/middlewares/jwt_auth.py
+++ mashina/middlewares/jwt_auth.py
@@ -1,16 +1,9 @@
 import jwt
-import redis
 from config import settings
 
 
 class JWTAuthMiddleware(object):
     def process_resource(self, req, resp, resource, params):
-        # try:
-        #     r = redis.StrictRedis(host='redis', db=0)
-        #     token = req.auth.split(' ')
-        #     token = r.get(token[1])
-        # except:
-        #     token = None
         token = req.auth
         if token is not None:
             req.context['auth'] = jwt.decode(token, settings.JWT_SECRET_KEY, algorithm='HS256')
