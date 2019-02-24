f4d5911d6d15cb3941dee84f345d1ee62a05b541
ezdookie <brian@bleax.com>
2019-02-24 13:11:19 -0500

    add environment variables

--- mashina/db.py
+++ mashina/db.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
+from config import settings
 
-engine = create_engine('postgresql://postgres@db:5432/postgres', echo=False)
+
+str_conn = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(
+    user=settings.DATABASE['db_user'],
+    password=settings.DATABASE['db_password'],
+    host=settings.DATABASE['db_host'],
+    port=settings.DATABASE['db_port'],
+    db_name=settings.DATABASE['db_name']
+)
+
+engine = create_engine(str_conn, echo=False)
 
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
