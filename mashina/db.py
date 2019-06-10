3e0ff56539653538c88edb8ed70c043e5b4b6cfc
ezdookie <brian@bleax.com>
2019-06-10 09:30:53 -0500

    get conn string from one line

--- mashina/db.py
+++ mashina/db.py
@@ -2,16 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from config import settings
 
-
-str_conn = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(
-    user=settings.DATABASE['db_user'],
-    password=settings.DATABASE['db_password'],
-    host=settings.DATABASE['db_host'],
-    port=settings.DATABASE['db_port'],
-    db_name=settings.DATABASE['db_name']
-)
-
-engine = create_engine(str_conn, echo=settings.DEBUG)
-
+engine = create_engine(settings.DATABASE_URL, echo=settings.DEBUG)
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
