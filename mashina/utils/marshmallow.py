d35f6d45d7f592c5cf2e0e3de4bd00b88c71e191
ezdookie <brian@bleax.com>
2019-06-10 09:31:25 -0500

    improve serializing

--- mashina/utils/marshmallow.py
+++ mashina/utils/marshmallow.py
@@ -1,55 +1,55 @@
-from marshmallow import validate, utils, class_registry
+from marshmallow import class_registry
 from marshmallow.base import SchemaABC
-from marshmallow.utils import missing as missing_
 from marshmallow.compat import basestring
 from marshmallow.fields import Nested
 
 _RECURSIVE_NESTED = 'self'
 
 
-class OptCachedNested(Nested):
-
-    def __init__(self, nested, cached=True, *args, **kwargs):
-        self.nested = nested
-        self.cached = cached
-        self.__schema = None
-        super(OptCachedNested, self).__init__(nested, *args, **kwargs)
-
+class CustomNested(Nested):
     @property
     def schema(self):
-        if not self.__schema or not self.cached:
-            # Ensure that only parameter is a tuple
-            if isinstance(self.only, basestring):
-                only = (self.only,)
-            else:
-                only = self.only
+        # Ensure that only parameter is a tuple
+        if isinstance(self.only, basestring):
+            only = (self.only,)
+        else:
+            only = self.only
 
-            # Inherit context from parent.
-            context = getattr(self.parent, 'context', {})
-            if isinstance(self.nested, SchemaABC):
-                self.__schema = self.nested
-                self.__schema.context.update(context)
-            elif isinstance(self.nested, type) and \
-                    issubclass(self.nested, SchemaABC):
-                self.__schema = self.nested(many=self.many,
+        # Inherit context from parent.
+        context = getattr(self.parent, 'context', {})
+        if isinstance(self.nested, SchemaABC):
+            _schema = self.nested
+            _schema.context.update(context)
+        elif isinstance(self.nested, type) and \
+                issubclass(self.nested, SchemaABC):
+            _schema = self.nested(many=self.many,
+                    only=only, exclude=self.exclude, context=context,
+                    load_only=self._nested_normalized_option('load_only'),
+                    dump_only=self._nested_normalized_option('dump_only'))
+        elif isinstance(self.nested, basestring):
+            if self.nested == _RECURSIVE_NESTED:
+                parent_class = self.parent.__class__
+                _schema = parent_class(many=self.many, only=only,
+                        exclude=self.exclude, context=context,
+                        load_only=self._nested_normalized_option('load_only'),
+                        dump_only=self._nested_normalized_option('dump_only'))
+            else:
+                schema_class = class_registry.get_class(self.nested)
+                _schema = schema_class(many=self.many,
                         only=only, exclude=self.exclude, context=context,
                         load_only=self._nested_normalized_option('load_only'),
                         dump_only=self._nested_normalized_option('dump_only'))
-            elif isinstance(self.nested, basestring):
-                if self.nested == _RECURSIVE_NESTED:
-                    parent_class = self.parent.__class__
-                    self.__schema = parent_class(many=self.many, only=only,
-                            exclude=self.exclude, context=context,
-                            load_only=self._nested_normalized_option('load_only'),
-                            dump_only=self._nested_normalized_option('dump_only'))
-                else:
-                    schema_class = class_registry.get_class(self.nested)
-                    self.__schema = schema_class(many=self.many,
-                            only=only, exclude=self.exclude, context=context,
-                            load_only=self._nested_normalized_option('load_only'),
-                            dump_only=self._nested_normalized_option('dump_only'))
-            else:
-                raise ValueError('Nested fields must be passed a '
-                                 'Schema, not {0}.'.format(self.nested.__class__))
-            self.__schema.ordered = getattr(self.parent, 'ordered', False)
-        return self.__schema
+        else:
+            raise ValueError('Nested fields must be passed a '
+                             'Schema, not {0}.'.format(self.nested.__class__))
+        _schema.ordered = getattr(self.parent, 'ordered', False)
+        return _schema
+
+    def get_value(self, *args, **kwargs):
+        self.attribute = None
+        return super(CustomNested, self).get_value(*args, **kwargs)
+
+    def deserialize(self, *args, **kwargs):
+        if not self.many:
+            self.attribute = '%s_id' % self.name
+        return super(CustomNested, self).deserialize(*args, **kwargs)
--- mashina/utils/serialize.py
+++ mashina/utils/serialize.py
@@ -1,10 +1,10 @@
-from mashina.utils.marshmallow import OptCachedNested
+from mashina.utils.marshmallow import CustomNested
 
 
 def get_nested_fields(Schema, parent=None):
     nested = []
     for f_n, f_o in Schema._declared_fields.items():
-        if type(f_o) == OptCachedNested:
+        if hasattr(f_o, 'nested'):
             field = f_n if not parent else '%s.%s' % (parent, f_n)
             nested.append(field)
             nested.extend(get_nested_fields(f_o.schema, parent=field))
