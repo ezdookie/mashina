a69819be681c2552581d1fb3cabad0e2fa52a0ea
Brian <brian@bleax.com>
2019-06-25 17:25:36 -0500

    add timestamp mixin

--- mashina/models/mixins.py
+++ mashina/models/mixins.py
@@ -1,6 +1,8 @@
 import uuid
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declared_attr
 from mashina.models.custom import GUID
 
 
@@ -10,3 +12,13 @@ class ModelIntegerIDMixin(object):
 
 class ModelGUIDMixin(object):
     id = Column(GUID, primary_key=True, default=uuid.uuid4)
+
+
+class ModelTimestampMixin(object):
+    @declared_attr
+    def created_at(self):
+        return Column(DateTime, server_default=func.now())
+
+    @declared_attr
+    def updated_at(self):
+        return Column(DateTime, onupdate=func.now())
