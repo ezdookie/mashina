8e17fee37e78d60a41bb54260ef854c6a2582a3c
ezdookie <brian@bleax.com>
2019-07-06 21:49:03 -0500

    update lib versions

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='mashina',
+setup(
+    name='mashina',
     version='1.0.0',
     install_requires=[
-        'falcon==1.4.1',
-        'SQLAlchemy==1.2.10',
-        'click==6.7',
-        'marshmallow==2.15.4',
-        'marshmallow-sqlalchemy==0.14.1'
+        'falcon==2.0.0',
+        'SQLAlchemy==1.3.5',
+        'click==7.0',
+        'marshmallow==2.19.5',
+        'marshmallow-sqlalchemy==0.17.0',
+        'PyJWT==1.7.1'
     ],
     packages=find_packages()
 )
